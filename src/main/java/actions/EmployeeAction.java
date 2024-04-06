76cedf6ee3447488b68f6420fea908c92e4db4a4
shogo.tatsukawa <kuroko405gm0112@gmail.com>
2024-04-06 23:16:50 +0900

    従業員destroyアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -176,7 +176,7 @@ public class EmployeeAction extends ActionBase {
         putRequestScope(AttributeConst.TOKEN, getTokenId());  // CSRF対策用トークン
         putRequestScope(AttributeConst.EMPLOYEE, ev);  // 取得した従業員情報
 
-        //編集画面を表示する
+        // 編集画面を表示する
         forward(ForwardConst.FW_EMP_EDIT);
     }
 
@@ -188,9 +188,9 @@ public class EmployeeAction extends ActionBase {
      */
     public void update() throws ServletException, IOException {
 
-        //CSRF対策 tokenのチェック
+        // CSRF対策 tokenのチェック
         if (checkToken()) {
-            //パラメータの値を元に従業員情報のインスタンスを作成する
+            // パラメータの値を元に従業員情報のインスタンスを作成する
             EmployeeView ev = new EmployeeView(
                     toNumber(getRequestParam(AttributeConst.EMP_ID)),
                     getRequestParam(AttributeConst.EMP_CODE),
@@ -201,30 +201,52 @@ public class EmployeeAction extends ActionBase {
                     null,
                     AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
 
-            //アプリケーションスコープからpepper文字列を取得
+            // アプリケーションスコープからpepper文字列を取得
             String pepper = getContextScope(PropertyConst.PEPPER);
 
-            //従業員情報更新
+            // 従業員情報更新
             List<String> errors = service.update(ev, pepper);
 
             if (errors.size() > 0) {
-                //更新中にエラーが発生した場合
+                // 更新中にエラーが発生した場合
 
-                putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
-                putRequestScope(AttributeConst.EMPLOYEE, ev); //入力された従業員情報
-                putRequestScope(AttributeConst.ERR, errors); //エラーのリスト
+                putRequestScope(AttributeConst.TOKEN, getTokenId());  // CSRF対策用トークン
+                putRequestScope(AttributeConst.EMPLOYEE, ev);  // 入力された従業員情報
+                putRequestScope(AttributeConst.ERR, errors);  // エラーのリスト
 
-                //編集画面を再表示
+                // 編集画面を再表示
                 forward(ForwardConst.FW_EMP_EDIT);
             } else {
-                //更新中にエラーがなかった場合
+                // 更新中にエラーがなかった場合
 
-                //セッションに更新完了のフラッシュメッセージを設定
+                // セッションに更新完了のフラッシュメッセージを設定
                 putSessionScope(AttributeConst.FLUSH, MessageConst.I_UPDATED.getMessage());
 
-                //一覧画面にリダイレクト
+                // 一覧画面にリダイレクト
                 redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
             }
+        }
+    }
+
+
+    /**
+     * 論理削除を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void destroy() throws ServletException, IOException {
+
+        // CSRF対策 tokenのチェック
+        if (checkToken()) {
+
+            // idを条件に従業員データを論理削除する
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+            // セッションに削除完了のフラッシュメッセージを設定
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+
+            // 一覧画面にリダイレクト
+            redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
         }
     }
 
