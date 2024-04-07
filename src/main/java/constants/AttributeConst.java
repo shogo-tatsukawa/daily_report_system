53471804557064b5de504a2db547c9e73bd2f091
shogo.tatsukawa <kuroko405gm0112@gmail.com>
2024-04-07 10:19:01 +0900

    ログインアクションの作成

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -21,6 +21,12 @@ public enum AttributeConst {
     ERR("errors"),
 
     // ログイン中の従業員
+    LOGIN_EMP("login_employee"),
+
+    //ログイン画面
+    LOGIN_ERR("loginError"),
+
+    //従業員管理
     EMPLOYEE("employee"),
     EMPLOYEES("employees"),
     EMP_COUNT("employees_count"),
