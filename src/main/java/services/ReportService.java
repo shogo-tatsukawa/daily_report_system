f433bef6fb582565a2669c8e4d46e11b8b236912
shogo.tatsukawa <kuroko405gm0112@gmail.com>
2024-04-07 15:56:55 +0900

    トップページアクションの作成

--- src/main/java/services/ReportService.java
+++ src/main/java/services/ReportService.java
@@ -27,6 +27,7 @@ public class ReportService extends ServiceBase {
 
        List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL_MINE, Report.class)
                .setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
+               .setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
                .setMaxResults(JpaConst.ROW_PER_PAGE)
                .getResultList();
        return ReportConverter.toViewList(reports);
@@ -40,7 +41,7 @@ public class ReportService extends ServiceBase {
     */
    public long countAllMine(EmployeeView employee) {
 
-       long count = (long) em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, long.class)
+       long count = (long) em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, Long.class)
                .setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
                .getSingleResult();
 
